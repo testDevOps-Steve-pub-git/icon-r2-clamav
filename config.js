f4dbf9feb3228e45a5057b84e083e57111ccebf5
shifeng Jin <shifeng.jin@ontario.ca>
2017-05-31 14:37:35 -0400

    property build

--- config.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-
-module.exports= {
-    clamav: {
-    endPoint: process.env.CLAMAV_ENDPOINT || "127.0.0.1", // ip of clamd 
-    port: process.env.CLAMAV_PORT || 3310,
-    timeout: process.env.CLAMAV_TIMEOUT || 10000 // 10s before considering it a failed attempt
-  }
-}
