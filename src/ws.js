1414e1e8c3fb8a70435ffd0ac0f5239d0d5a9c7b
shifeng Jin <shifeng.jin@ontario.ca>
2017-06-21 10:41:52 -0400

    remove actively heartbeat delivery

--- src/ws.js
+++ src/ws.js
@@ -33,41 +33,16 @@ module.exports = wssStart = (endpoint,restartTime,clamdConfig) => {
         return
     }
     var msgMap = { "init": "init", "reset": "reset", "heartbeat": "heartbeat", "update": 'update' }
-    let timer = 0
     wss.onopen = (event) => {
         logger.log(processType, "Connection to Virus Update Controll server is open")
         // send initialization detail
         wss.send(JSON.stringify({ "type": msgMap['init'], "identifier": identifier, "detail": '' }))
-
-        // send status update of clam deadmon every minute
-        timer = setInterval(() => {
-            logger.debug(processType, "Sending heartbeat to updated controll server")
-            clamd.ping(clamdConfig.port, clamdConfig.endPoint, clamdConfig.timeout).then((result) => {
-                if (wss.readyState === WebSocket.OPEN) {
-                    wss.send(JSON.stringify({ "type": msgMap['heartbeat'], "identifier": identifier, "detail": 'green' }))
-                } else {
-                    logger.log(processType, "Communication tunnel accidentally closed, please wait for connection reset.")
-                }
-
-            }, (reason) => {
-                if (wss.readyState === WebSocket.OPEN) {
-                    wss.send(JSON.stringify({ "type": msgMap['heartbeat'], "identifier": identifier, "detail": 'red' }))
-                } else {
-                    logger.log(processType, "Communication tunnel accidentally closed, please wait for connection reset.")
-                }
-
-            }
-            )
-
-        }, 60000)
     }
     wss.onerror = (err) => {
-        clearInterval(timer)
+
         logger.error(processType, "Connection to updated server failed: " + err)
     }
     wss.onclose = (event) => {
-        // delete timer, release resource
-        clearInterval(timer)
         logger.log(processType, "Connection to Virus Update Controll will be re-established in " + restartTime + " seconds ")
         setTimeout(() => {
             wssStart(endpoint,restartTime,clamdConfig)
@@ -84,7 +59,7 @@ module.exports = wssStart = (endpoint,restartTime,clamdConfig) => {
                 logger.log(processType, 'connection re-established with update controll server at: ' + endpoint)
                 break
             case msgMap['heartbeat']:
-                logger.debug(processType, "Manually deliver heartbeat to update controll server")
+                logger.debug(processType, "On heartbeat request")
                 clamd.ping(clamdConfig.port, clamdConfig.endPoint, clamdConfig.timeout).then((result) => {
                     if (wss.readyState === WebSocket.OPEN) {
                         wss.send(JSON.stringify({ "type": msgMap['heartbeat'], "identifier": identifier, "detail": 'green' }))
@@ -103,7 +78,7 @@ module.exports = wssStart = (endpoint,restartTime,clamdConfig) => {
 
                 break;
             case msgMap['update']:
-                logger.debug(processType, "received virus database update signal from updated controll server")
+                logger.debug(processType, "On virus database update request")
                 if (wss.readyState === WebSocket.OPEN) {
                     wss.send(JSON.stringify({ "type": msgMap['update'], "identifier": identifier, "detail": { "updating": true, "updatingError": false } }))
                 } else {
